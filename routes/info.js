4af6cdffc4b23342ce0f4c525a929bae12f42ccf
Uzair Tariq <uzair0213@icloud.com>
2020-01-30 16:35:12 -0700

    Add ability to automatically get the route name from routeNum input

--- routes/info.js
+++ routes/info.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 const routeData = require('../public/data/routes');
 
-// TODO: Find a way to find the index automatically
-const routeIndex = 164;
-const routeName = routeData.data[routeIndex][10];
-
 /* GET users listing. */
 router.get('/', function(req, res) {
+  let routeIndex = 0;
+  for (let i = 0; i < routeData.data.length; i++) {
+    if (routeData.data[i][8] === req.query.routeNumber) {
+      routeIndex = i;
+      break;
+    }
+  }
+  const routeName = routeData.data[routeIndex][10];
+
   res.render('info', {
     routeNum: req.query.routeNumber,
     name: routeName,
