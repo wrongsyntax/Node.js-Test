2ab4f1970496ad899a3a22cd033f70a4017109d0
Uzair Tariq <uzair0213@icloud.com>
2020-01-29 20:35:52 -0700

    Get input from index page to info page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/info');
+const infoRouter = require('./routes/info');
 
 const app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/info', usersRouter);
+app.use('/info', infoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/info.js
+++ routes/info.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('info', {routeNum: req.body.routeNum});
+  let input = req.query.routeNumber;
+  console.log(input);
+  res.render('info', {routeNum: input});
 });
 
 module.exports = router;
