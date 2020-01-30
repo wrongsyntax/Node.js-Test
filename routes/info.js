6d984214a0e36cf538efca57c2ed2d1eae879cdf
Uzair Tariq <uzair0213@icloud.com>
2020-01-30 16:14:19 -0700

    Update to get route name from routes.json instead of user input

--- routes/info.js
+++ routes/info.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const routeData = require('../public/data/routes');
+
+// TODO: Find a way to find the index automatically
+const routeIndex = 164;
+const routeName = routeData.data[routeIndex][10];
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  let input = req.query.routeNumber;
-  console.log(input);
-  res.render('info', {routeNum: input});
+  res.render('info', {
+    routeNum: req.query.routeNumber,
+    name: routeName,
+    title: 'jatw - info'
+  });
 });
 
 module.exports = router;
