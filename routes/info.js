8f99234c03f688597e2e125837846ac9864875a1
Uzair Tariq <uzair0213@icloud.com>
2020-01-30 17:41:24 -0700

    Add default fallback to routes.json in case the route entered doesn't exist

--- routes/info.js
+++ routes/info.js
@@ -4,7 +4,8 @@ const routeData = require('../public/data/routes');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  let routeIndex = 0;
+  // Get the route name from routes.json based on routeNum
+  let routeIndex = 222;    // Originally 222 since index 222 is the fallback if the route is not found
   for (let i = 0; i < routeData.data.length; i++) {
     if (routeData.data[i][8] === req.query.routeNumber) {
       routeIndex = i;
